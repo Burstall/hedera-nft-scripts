b092412584c95c5ea0bd918aa518af4d1d1d0d0b
Stowerling <richard@alphacenturi.com>
2022-10-01 16:24:59 +0100

    added approval spend

--- transferHbar.js
+++ transferHbar.js
@@ -15,19 +15,24 @@ const operatorKey = PrivateKey.fromString(process.env.MY_PRIVATE_KEY);
 const env = process.env.ENVIRONMENT || null;
 const readlineSync = require('readline-sync');
 let client;
+let isApproval = false;
+let multiSig = false;
+let onBehalfOfAccount;
 
 async function main() {
 	if (getArgFlag('h')) {
-		console.log('Usage: node transferHbar.js -rec 0.0.XXXX -amt Z [-memo \'ABC DEF\' ] [-multisig]');
+		console.log('Usage: node transferFT.js -rec 0.0.XXXX -amt Z [-memo \'ABC DEF\' ] [-multisig] [-approval 0.0.YYY]');
 		// console.log('Usage: node transferHbar.js [-sender 0.0.ZZZ] -rec 0.0.XXXX -amt Z [-memo \'ABC DEF\' ] [-multisig]');
 		// console.log('       -sender 		overide the account sending - will require additional signatures');
 		console.log('       -rec 			address of the receving account');
 		console.log('       -amt 			amount to send');
 		console.log('       -multisig		flag to look for multisig signing');
+		console.log('       -approval		spend hbar from the account assuming allowance approved');
 		process.exit(0);
 	}
 
-	let multiSig = getArgFlag('multisig');
+	multiSig = getArgFlag('multisig');
+	isApproval = getArgFlag('approval');
 	const memo = getArg('memo');
 
 	let sender;
@@ -64,6 +69,11 @@ async function main() {
 	console.log('- Amount:', new Hbar(amount).toString());
 	console.log('- Using ENVIRONMENT:', env);
 
+	if (isApproval) {
+		onBehalfOfAccount = AccountId.fromString(getArg('approval'));
+		console.log('- Approval spend on behalf of:', onBehalfOfAccount.toString());
+	}
+
 	if (env == 'TEST') {
 		client = Client.forTestnet();
 		console.log('Transfer hbar in *TESTNET*');
@@ -79,10 +89,10 @@ async function main() {
 
 	client.setOperator(operatorId, operatorKey);
 
-	const proceed = readlineSync.keyInYNStrict('Do you want to maske the transfer?');
+	const proceed = readlineSync.keyInYNStrict('Do you want to make the transfer?');
 
 	if (proceed) {
-		const result = await transferHbarFcn(sender, receiver, amount, memo, multiSig);
+		const result = await transferHbarFcn(sender, receiver, amount, memo);
 
 		if (result) {
 			console.log('\n-Transfer completed');
@@ -107,16 +117,22 @@ async function main() {
  * @param {boolean} multiSig default false, set true if to spit out bytes and take the returned signed ones
  * @returns {boolean} outcome of the requested transfer
  */
-async function transferHbarFcn(sender, receiver, amount, memo = null, multiSig = false) {
+async function transferHbarFcn(sender, receiver, amount, memo = null) {
 	// add signature documented to require only single node to be used.
 	const nodeId = [];
 	nodeId.push(new AccountId(3));
 
 	const transferTx = new TransferTransaction()
 		.addHbarTransfer(receiver, amount)
-		.addHbarTransfer(sender, -amount)
 		.setNodeAccountIds(nodeId)
 		.setTransactionId(TransactionId.generate(operatorId));
+
+	if (isApproval) {
+		transferTx.addApprovedHbarTransfer(onBehalfOfAccount, -amount);
+	}
+	else {
+		transferTx.addHbarTransfer(sender, -amount);
+	}
 
 	if (memo) {
 		transferTx.setTransactionMemo(memo);
@@ -127,12 +143,6 @@ async function transferHbarFcn(sender, receiver, amount, memo = null, multiSig =
 	let transferSigned;
 
 	if (multiSig) {
-		// from testing having the operator / fee payer as sep account
-		// add the known signature
-		// const knownSig = (await transferTx.sign(operatorKey)).toBytes();
-		// const knownPublicKey = operatorKey.publicKey;
-		// transferSigned = await transferTx.addSignature(knownPublicKey, knownSig);
-
 		// request other signatures
 		transferSigned = await requestMultiSig(transferTx);
 	}
@@ -171,4 +181,11 @@ function getArgFlag(arg) {
 }
 
 
-main();
+main()
+	.then(() => {
+		process.exit(0);
+	})
+	.catch(error => {
+		console.error(error);
+		process.exit(1);
+	});
