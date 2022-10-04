4677780cb0059063e371d61c2c3bddb2276672ee
Stowerling <richard@alphacenturi.com>
2022-10-04 20:53:27 +0100

    added functionality to scan for dragons owned by single wallet

--- hederianDragonsTCGLeaderboard.mjs
+++ hederianDragonsTCGLeaderboard.mjs
@@ -2,14 +2,17 @@ import fetch from 'cross-fetch';
 
 // exclude (or label?) community wallet
 const excludedWallets = ['0.0.859990'];
+const dragonsOriginsTokenId = '0.0.1003963';
 const dragonsTCGTokenId = '0.0.1003996';
 const dragonsNPCTokenId = '0.0.1057949';
 const zuseEscrow = '0.0.690356';
 const hashGuildEscrow = '0.0.1007535';
 const maxRetries = 20;
 const ipfsGateways = ['https://cloudflare-ipfs.com/ipfs/', 'https://ipfs.eth.aragon.network/ipfs/', 'https://ipfs.io/ipfs/', 'https://ipfs.eternum.io/ipfs/', 'https://cloudflare-ipfs.com/ipfs/'];
-const serialNameMap = new Map();
+let npcSerialsNameMap = new Map();
 let totalCompleted = 0;
+let dragonMap = new Map();
+let maxNameLength = 0;
 let verbose;
 
 async function fetchJson(url, depth = 0) {
@@ -55,6 +58,8 @@ async function getSerialNFTAttribs(tokenId) {
 
 	const promisesList = [];
 
+	const serialNameMap = new Map();
+
 	do {
 		const json = await fetchJson(baseUrl + routeUrl);
 		if (json == null) {
@@ -82,6 +87,22 @@ async function getSerialNFTAttribs(tokenId) {
 			console.log(`ERROR: element ${r} -> ${responseData[r]}`);
 		}
 	}
+
+	return serialNameMap;
+}
+
+/**
+ * Pull the origin dragons to create an empty list
+ * @returns {Map}
+ */
+async function prepareEmptyDragonMap() {
+	dragonMap = await getSerialNFTAttribs(dragonsOriginsTokenId);
+	const emptyDragonOwnershipMap = new Map();
+	for (const dragonName of dragonMap.values()) {
+		if (dragonName.length > maxNameLength) maxNameLength = dragonName.length;
+		emptyDragonOwnershipMap.set(dragonName, 0);
+	}
+	return emptyDragonOwnershipMap;
 }
 
 async function fetchIPFSJson(ifpsUrl, depth = 0, seed = 0) {
@@ -163,10 +184,61 @@ function getDragonFromSerial(tokenId, serial) {
 		else if (serial <= 800) return 'Sacrifice';
 		else return 'UKNOWN';
 		*/
-		return serialNameMap.get(serial) || 'UNKNOWN';
+		return npcSerialsNameMap.get(serial) || 'UNKNOWN';
+	case dragonsOriginsTokenId:
+		return dragonMap.get(((serial - (serial % 10)) / 10) + 1);
 	default:
 		console.log(`Unknown classification for ${tokenId}/#${serial}`);
 	}
+}
+
+/**
+ * Function to pull an owners TCG cards
+ * @param {string} wallet
+ * @param {string} tokenId
+ * @returns {Map}
+ */
+async function getUsersDragonTCGs(wallet, tokenId) {
+	// base URL
+	const baseUrl = 'https://mainnet-public.mirrornode.hedera.com';
+	let routeUrl = `/api/v1/tokens/${tokenId}/nfts/?account.id=${wallet}`;
+	if (verbose) { console.log(baseUrl + routeUrl);}
+
+	const ownedMap = await prepareEmptyDragonMap();
+
+	let batch = 0;
+	do {
+		batch++;
+		const json = await fetchJson(baseUrl + routeUrl);
+		if (json == null) {
+			console.log('FATAL ERROR: no NFTs found', baseUrl + routeUrl);
+			// unlikely to get here but a sensible default
+			return;
+		}
+		const nfts = json.nfts;
+
+		for (let n = 0; n < nfts.length; n++) {
+			if (verbose) console.log(`Batch ${batch} - Processing item:', ${n}, 'of', ${nfts.length}`);
+			const value = nfts[n];
+			const serial = value.serial_number;
+
+
+			if (value.deleted) continue;
+			const nftOwner = value.account_id;
+			if (excludedWallets.includes(nftOwner) || nftOwner == zuseEscrow || nftOwner == hashGuildEscrow) {
+				continue;
+			}
+			const dragonName = await getDragonFromSerial(dragonsOriginsTokenId, serial);
+			let amtOwned = ownedMap.get(dragonName) || 0;
+			amtOwned++;
+			ownedMap.set(dragonName, amtOwned);
+		}
+
+		routeUrl = json.links.next;
+	}
+	while (routeUrl);
+
+	return ownedMap;
 }
 
 async function getUniqueDragonTCGOwnershipMap(tokenId, nftOwnerMap = new Map()) {
@@ -213,35 +285,71 @@ async function getUniqueDragonTCGOwnershipMap(tokenId, nftOwnerMap = new Map()) 
 	return nftOwnerMap;
 }
 
+function getArg(arg) {
+	const customIndex = process.argv.indexOf(`-${arg}`);
+	let customValue;
+
+	if (customIndex > -1) {
+		// Retrieve the value after --custom
+		customValue = process.argv[customIndex + 1];
+	}
+
+	return customValue;
+}
+
+function getArgFlag(arg) {
+	const customIndex = process.argv.indexOf(`-${arg}`);
+
+	if (customIndex > -1) {
+		return true;
+	}
+
+	return false;
+}
+
 async function main() {
 	verbose = false;
-	// parse token for serial / name pairing [on non dragon TC tokens]
-	console.log('Processing non-dragon card metadata...');
-	await getSerialNFTAttribs(dragonsNPCTokenId);
-	console.log('**COMPLETE**');
-	// get a map of owners per serial
-	console.log('Parsing ownership of Dragon TCs');
-	let nftOwnerMap = await getUniqueDragonTCGOwnershipMap(dragonsTCGTokenId);
-	console.log('Parsing ownership of non-dragon TCs');
-	nftOwnerMap = await getUniqueDragonTCGOwnershipMap(dragonsNPCTokenId, nftOwnerMap);
-	console.log('**COMPLETE**');
+	if (getArgFlag('h')) {
+		console.log('Usage: node hederianDragonsTCGLederboard.mjs [-wallet 0.0.XXX]');
+		console.log('					-wallet 0.0.XXX		Process dragon TCG holding');
+		console.log('					otherwise (default) builds the leaderboard');
+	}
 
-	console.log('Preparing Leaderboard');
-	const nftOwnerList = [];
-	nftOwnerMap.forEach((value, key) => {
-		nftOwnerList.push([key, value.length]);
-	});
-
-	const sortedList = nftOwnerList.sort((a, b) => {
-		if (a[1] == b[1]) {
-			return a[0] - b[0];
+	if (getArgFlag('wallet')) {
+		const ownershipMap = await getUsersDragonTCGs(getArg('wallet'), dragonsTCGTokenId);
+		for (const [dragon, qty] of ownershipMap) {
+			console.log(dragon.padEnd(maxNameLength), '\t', qty);
 		}
-		return b[1] - a[1];
-	});
+	}
+	else {
+		// parse token for serial / name pairing [on non dragon TC tokens]
+		console.log('Processing non-dragon card metadata...');
+		npcSerialsNameMap = await getSerialNFTAttribs(dragonsNPCTokenId);
+		console.log('**COMPLETE**');
+		// get a map of owners per serial
+		console.log('Parsing ownership of Dragon TCs');
+		let nftOwnerMap = await getUniqueDragonTCGOwnershipMap(dragonsTCGTokenId);
+		console.log('Parsing ownership of non-dragon TCs');
+		nftOwnerMap = await getUniqueDragonTCGOwnershipMap(dragonsNPCTokenId, nftOwnerMap);
+		console.log('**COMPLETE**');
 
-	console.log('LEADERBOARD\nRank\tWallet\t\tUnique Owned');
-	for (let w = 0; w < sortedList.length; w++) {
-		console.log(`${w + 1}\t${nftOwnerList[w][0]}\t\t${nftOwnerList[w][1]}`);
+		console.log('Preparing Leaderboard');
+		const nftOwnerList = [];
+		nftOwnerMap.forEach((value, key) => {
+			nftOwnerList.push([key, value.length]);
+		});
+
+		const sortedList = nftOwnerList.sort((a, b) => {
+			if (a[1] == b[1]) {
+				return a[0] - b[0];
+			}
+			return b[1] - a[1];
+		});
+
+		console.log('LEADERBOARD\nRank\tWallet\t\tUnique Owned');
+		for (let w = 0; w < sortedList.length; w++) {
+			console.log(`${w + 1}\t${nftOwnerList[w][0]}\t\t${nftOwnerList[w][1]}`);
+		}
 	}
 }
 
