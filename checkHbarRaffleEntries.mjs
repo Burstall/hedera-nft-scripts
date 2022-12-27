79798040a9555c497f16f31748d32e56c02d4f46
Stowerling <richard@alphacenturi.com>
2022-12-27 20:45:52 +0000

    added basic hbar raffle screener

---
+++ checkHbarRaffleEntries.mjs
@@ -0,0 +1,195 @@
+import { fetch } from 'cross-fetch';
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+import { Hbar, HbarUnit } from '@hashgraph/sdk';
+dotenv.config();
+const baseUrl = process.env.MIRROR_NODE_BASEURL || 'https://mainnet-public.mirrornode.hedera.com';
+const maxRetries = Number(process.env.MAX_RETRY) || 3;
+let totalTxProcessed = 0;
+let totalTxEntries = 0;
+let excludeList = [];
+
+async function pullTransactions(pathUrl, wallet, accountBalanceMap = new Map()) {
+	try {
+		const data = await fetchJson(baseUrl + pathUrl);
+		// iterate through transactions checking time stamp in bounds and wallet too.
+		for (let i = 0; i < data.transactions.length; i++) {
+			totalTxProcessed++;
+			const tx = data.transactions[i];
+			if (tx.transfers) {
+				let amt = 0;
+				for (let t = 0; t < tx.transfers.length; t++) {
+					const transfer = tx.transfers[t];
+					if (transfer.amount > 0) continue;
+					const account = transfer.account;
+					amt += Number(new Hbar(-transfer.amount, HbarUnit.Tinybar).toBigNumber());
+					// only look at credits to the account
+					if (excludeList.includes(account)) continue;
+					if (account != wallet && amt > 0 && account != '0.0.800') {
+						let totalAmt = accountBalanceMap.get(account) || 0;
+						totalAmt += amt;
+						accountBalanceMap.set(account, totalAmt);
+						totalTxEntries++;
+						// console.log(account, amt, totalAmt);
+					}
+				}
+			}
+		}
+
+		if (data.links.next) {
+			return await pullTransactions(data.links.next, wallet, accountBalanceMap);
+		}
+
+	}
+	catch (err) {
+		console.error('ERROR looking up transactions:', baseUrl + pathUrl, err);
+	}
+
+	return accountBalanceMap;
+}
+
+async function fetchJson(url, depth = 0) {
+	if (depth >= maxRetries) return null;
+	depth++;
+	try {
+		const res = await fetchWithTimeout(url);
+		if (res.status != 200) {
+			// console.log(depth, url, res);
+			await sleep(1000 * depth);
+			return await fetchJson(url, depth);
+		}
+		return res.json();
+
+	}
+	catch (err) {
+		await sleep(1000 * depth);
+		return await fetchJson(url, depth);
+	}
+}
+
+async function fetchWithTimeout(resource, options = {}) {
+	const { timeout = 30000 } = options;
+
+	const controller = new AbortController();
+	const id = setTimeout(() => controller.abort(), timeout);
+	const response = await fetch(resource, {
+		...options,
+		signal: controller.signal,
+	});
+	clearTimeout(id);
+	return response;
+}
+
+function getArg(arg) {
+	const customIndex = process.argv.indexOf(`-${arg}`);
+	let customValue;
+
+	if (customIndex > -1) {
+		// Retrieve the value after --custom
+		customValue = process.argv[customIndex + 1];
+	}
+
+	return customValue;
+}
+
+function getArgFlag(arg) {
+	const customIndex = process.argv.indexOf(`-${arg}`);
+
+	if (customIndex > -1) {
+		return true;
+	}
+
+	return false;
+}
+
+const sleep = (milliseconds) => {
+	return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+async function main() {
+	if (getArgFlag('h')) {
+		console.log('usage: node checkHbarRaffleEntries.mjs -cost XX -start <epoch> -end <epoch> -wallet 0.0.ZZZ -token 0.0.QQQ [-exclude 0.0.XX,0.0.WW]');
+		console.log('				-cost 	the cost of a ticket in hbar');
+		console.log('				-start 	start time as EPOCH');
+		console.log('				-end 	end time as EPOCH');
+		console.log('				-wallet	wallet to search txs for');
+		console.log('				-exclude	wallets to exclude comma delimited');
+		console.log('https://www.epochconverter.com/ may be usueful for conversion');
+		process.exit(0);
+	}
+	const wallet = getArg('wallet');
+	const startTime = getArg('start');
+	const endTime = getArg('end');
+	const cost = getArg('cost');
+
+	if (getArgFlag('exclude')) {
+		excludeList = getArg('exclude').split(',');
+	}
+
+
+	console.log('Using:' +
+					'\nWallet: ' + wallet +
+					'\nExcluding: ' + excludeList +
+					'\nStart: ' + new Date(startTime * 1000).toISOString() +
+					'\nEnd: ' + new Date(endTime * 1000).toISOString() +
+					'\nTicket Cost: ' + cost +
+					'\nPaid in: hbar',
+	);
+
+	if (!wallet || !startTime || !endTime || !cost) {
+		console.log('Parameters missing - please check the command or run with -h for usage');
+		process.exit(1);
+	}
+
+	const pathUrl = `/api/v1/transactions?account.id=${wallet}&transactionType=cryptotransfer&type=credit&timestamp=gte:${startTime}&timestamp=lte:${endTime}`;
+	const acctBalMap = await pullTransactions(pathUrl, wallet);
+
+	console.log('Wallet\tPaid\tTickets\tWasted');
+	let totalAmount = 0;
+	let totalTickets = 0;
+	let wheelSpinString = '';
+	// acctBalMap.forEach((amt, acct) => {
+	for (const [acct, amt] of acctBalMap.entries()) {
+		const entries = Math.floor(amt / cost);
+		totalTickets += entries;
+		console.log(acct + '\t' + amt + '\t' + entries + '\t' + amt % cost);
+		totalAmount += amt;
+		for (let e = 0; e < entries; e++) {
+			wheelSpinString += acct + '\n';
+		}
+	}
+
+	console.log('Transactions processed: ' + totalTxProcessed);
+	console.log('\t of which valid entries: ' + totalTxEntries);
+	console.log('\ttotal hbar collected: ', totalAmount);
+	console.log('\t\ttickets: ', totalTickets);
+
+	try {
+
+		const outputTime = new Date();
+		const timestamp = outputTime.toISOString().split('.')[0].replaceAll(':', '-');
+		const filename = `./hbar-raffles-${timestamp}.txt`;
+
+		fs.writeFile(filename, wheelSpinString, { flag: 'w' }, function(err) {
+			if (err) {return console.error(err);}
+			// read it back in to be sure it worked.
+			fs.readFile(filename, 'utf-8', function(err) {
+				if (err) {
+					console.log('Reading file failed -- printing to console');
+					console.log(wheelSpinString);
+				}
+				console.log('Wheel spin file created', filename);
+			});
+		});
+	}
+	catch (err) {
+		// If we fail to write it then spit out the values to command line so not lost
+		console.log('Error writing file -- bailing', err);
+
+		console.log(wheelSpinString);
+		process.exit(1);
+	}
+
+}
+
+main();
